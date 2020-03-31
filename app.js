0ac46b2c4c0bea833ed9a5d123a8fad93d97ab9c
UmaCS <wiutumka@gmail.com>
2020-03-30 20:21:56 -0700

    js functions are added

--- app.js
+++ app.js
@@ -4,30 +4,45 @@ const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const confirmpass = document.querySelector('#confirmpass');
 
-// const subButton = document.getElementById('submit');
 
-// const inputObj = {
-//   username: username,
-//   email: email,
-//   password: password,
-//   confirmPass: confirmPass
-// }
 
-// ********** Main eventListener ***********
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  
+// show error function
+const showError = (input, messege) => {
+	const inputSection = input.parentElement;
+	inputSection.className = 'input-section error';
+	const small = inputSection.querySelector('small');
+	small.innerText = messege;
 }
-// subButton.addEventListener('click', e => {
-//   e.preventDefault();
-//   for(let input in inputObj){
-//     if(inputObj[input].value === ''){
-//       return inputObj[input].classList.add('inValidInput');
-//     }
-//     else if(inputObj[input].value.length > 0){
-//       return inputObj[input].classList.add('validInput');
-//     }
-//   }
-//
-// });
+
+//show success function
+const showSuccess = (input) => {
+	const inputSection = input.parentElement;
+	inputSection.className = 'input-section success';
+};
+
+// email validator function
+const validateEmail = (email) => {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
+
+// check every field function
+const checkFields = (inputArray) => {
+	inputArray.forEach(input => {
+		if (input.value === '') {
+			validateEmail(email);
+			showError(input, `${input.id} is required`)
+		} else {
+			validateEmail(email);
+			showSuccess(input);
+		}
+	})
+}
+
+// Main eventListener
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	checkFields([username, email, password, confirmpass])
+});
