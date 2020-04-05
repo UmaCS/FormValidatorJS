b36facd00b122457403dd757332741c2a78f92f6
UmaCS <wiutumka@gmail.com>
2020-04-05 06:20:06 -0700

    validating functions

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#form');
 const username = document.querySelector('#username');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const confirmpass = document.querySelector('#confirmpass');
+const confirmPassword = document.querySelector('#confirmPassword');
 
 
 
@@ -22,27 +22,40 @@ const showSuccess = (input) => {
 };
 
 // email validator function
-const validateEmail = (email) => {
-	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-	return re.test(String(email).toLowerCase());
+const validateEmail = email => {
+	const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if (re.test(email.value.trim())) {
+		showSuccess(email);
+	} else {
+		showError(email, "Invalid email!");
+	}
 }
 
 
 // check every field function
 const checkFields = (inputArray) => {
 	inputArray.forEach(input => {
-		if (input.value === '') {
-			validateEmail(email);
+		if (input.value.trim() === '') {
 			showError(input, `${input.id} is required`)
 		} else {
-			validateEmail(email);
 			showSuccess(input);
 		}
-	})
+	});
 }
 
-// Main eventListener
+// validate Passwords
+const checkPasswords = (input1, input2) => {
+	if (input1.value !== input2.value) {
+		showError(input2, 'Password did not match!');
+	}
+}
+
+
+//***** Main eventListener *****//
+
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
-	checkFields([username, email, password, confirmpass])
+	checkFields([username, email, password, confirmPassword]);
+	validateEmail(email);
+	checkPasswords(password, confirmPassword);
 });
